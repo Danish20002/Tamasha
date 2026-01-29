51beb09bb2671755baac36fd51010e6bf8437dba
Muhammad Danish Azeem <106392505+Danish20002@users.noreply.github.com>
2026-01-29 16:57:55 +0500

    Update iframe source URL for live TV stream

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const liveTvContainer = document.getElementById('live-tv-container');
     const iframe = document.createElement('iframe');
-    iframe.src = 'https://tamashaweb.com/psl-2025'; // Replace with your live TV stream URL
+    iframe.src = 'https://tamashaweb.com/psl-2026'; // Replace with your live TV stream URL
     iframe.width = '100%';
     iframe.height = '100%';
     iframe.frameBorder = '0';
